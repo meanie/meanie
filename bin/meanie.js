b97401be64c2ac9de4cf412f48fbb8c07bc4d8a5
Adam Buczynski <adambuczynski@gmail.com>
2015-06-18 18:48:33 +1200

    Update CLI tool

--- bin/meanie.js
+++ bin/meanie.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-
 'use strict';
+
+/**
+ * Module dependencies
+ */
 var chalk = require('chalk');
 var semver = require('semver');
 var Liftoff = require('liftoff');
@@ -11,18 +14,32 @@ var argv = require('minimist')(process.argv.slice(2));
 //Set env var for initial cwd before anything touches it
 process.env.INIT_CWD = process.cwd();
 
+/**
+ * Helper to check if we have a param in the arguments list
+ */
+function hasParam(command) {
+  if (argv._.indexOf(command) !== -1) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Helper to get parameters after command
+ */
+function getParamsAfter(command) {
+  var indexOf = argv._.indexOf(command);
+  if (indexOf === -1) {
+    return [];
+  }
+  return argv._.slice(indexOf+1);
+}
+
 //Create cli instance
+var cliPackage = require('../package');
 var cli = new Liftoff({
   name: 'meanie',
   v8flags: v8flags
-});
-
-//Exit with 0 or 1
-var failed = false;
-process.once('exit', function(code) {
-  if (code === 0 && failed) {
-    process.exit(1);
-  }
 });
 
 //Event listeners for CLI
@@ -30,13 +47,8 @@ cli.on('require', function(name) {
   console.log('Requiring external module', chalk.magenta(name));
 });
 cli.on('requireFail', function(name) {
-  console.log(chalk.red('Failed to load external module'), chalk.magenta(name));
+  console.error(chalk.red('Failed to load external module', name));
 });
-
-//Parse arguments
-var cliPackage = require('../package');
-var versionFlag = argv.v || argv.version;
-var installFlag = argv.i || argv.install;
 
 //Launch CLI
 cli.launch({}, handleArguments);
@@ -45,21 +57,15 @@ cli.launch({}, handleArguments);
 function handleArguments(env) {
 
   //Output version
-  if (versionFlag) {
-
-    //CLI version
+  if (argv.v || argv.version) {
     console.log(
-      chalk.magenta('Meanie'), 'CLI version', chalk.magenta(cliPackage.version)
+      chalk.magenta('Meanie'), 'global version', chalk.magenta(cliPackage.version)
     );
-
-    //Local version
     if (env.modulePackage && typeof env.modulePackage.version !== 'undefined') {
       console.log(
         chalk.magenta('Meanie'), 'local version', chalk.magenta(env.modulePackage.version)
       );
     }
-
-    //Exit
     process.exit(0);
   }
 
@@ -72,11 +78,11 @@ function handleArguments(env) {
     process.exit(1);
   }
 
-  //Check for semver difference between cli and local installation
+  //Check for version difference between cli and local installation
   if (semver.gt(cliPackage.version, env.modulePackage.version)) {
     console.log(chalk.red('Warning: meanie version mismatch:'));
-    console.log(chalk.red('Global meanie is', cliPackage.version));
-    console.log(chalk.red('Local meanie is', env.modulePackage.version));
+    console.log(chalk.red('Global version is', cliPackage.version));
+    console.log(chalk.red('Local version is', env.modulePackage.version));
   }
 
   //Chdir if needed
@@ -91,11 +97,21 @@ function handleArguments(env) {
   //Get meanie instance
   var Meanie = require(env.modulePath);
 
-  //Install packages
-  if (installFlag) {
-    var toInstall = argv._.splice(0);
+  //For quick development, override with global meanie
+  Meanie = require('../index');
+
+  //Create new Meanie project in current directory
+  if (hasParam('create')) {
     process.nextTick(function() {
-      Meanie.install.apply(Meanie, toInstall);
+      Meanie.init();
+    });
+  }
+
+  //Install Meanie plugins in current directory
+  else if (hasParam('install')) {
+    var toInstall = getParamsAfter('install');
+    process.nextTick(function() {
+      Meanie.install(toInstall);
     });
   }
 }
--- index.js
+++ index.js
@@ -3,36 +3,234 @@
 /**
  * Module dependencies
  */
+var fs = require('fs');
+var os = require('os');
+var del = require('del');
+var cpr = require('cpr');
 var chalk = require('chalk');
+var tildify = require('tildify');
+var Git = require('nodegit');
+
+/*var RegistryClient = require('npm-registry-client');
+var client = new RegistryClient();
+var uri = "https://registry.npmjs.org/meanie-core";
+var params = {timeout: 1000};*/
+
+//client.get(uri, params, function (error, data, raw, res) {
+  // error is an error if there was a problem.
+  // data is the parsed data object
+  // raw is the json string
+  // res is the response from couch
+//})
+
+/*****************************************************************************
+ * Helpers
+ ***/
 
 /**
- * Constructor
+ * Get temporary directory for given module
  */
-function Meanie() {
+function getTempDir(module) {
 
+  //Prepare temporary directory
+  var tmpDir = os.tmpDir() + 'meanie' + (module ? ('.' + module) : '');
+  if (fs.existsSync(tmpDir)) {
+    del.sync(tmpDir, {
+      force: true
+    });
+  }
+
+  //Create it
+  fs.mkdirSync(tmpDir);
+  if (!fs.existsSync(tmpDir)) {
+    return '';
+  }
+
+  //Return it
+  return tmpDir;
 }
 
 /**
- * Install a meanie plugin
+ * Get repo URL for given module
+ * TODO: read from npm registry
  */
-Meanie.prototype.install = function(plugin) {
+function getRepoUrl(module) {
+  return 'https://github.com/meanie/meanie-' + module;
+}
 
-  //Array of plugins given
-  if (Array.isArray(plugin)) {
-    plugin.forEach(function(plugin) {
-      this.install(plugin);
-    }.bind(this));
-    return;
+/**
+ * Clone module from repository
+ */
+function cloneFromRepo(module, cb) {
+
+  //Get temporary directory
+  var tmpDir = Meanie.util.getTempDir(module);
+  if (!tmpDir) {
+    return cb(new Error('Could not create temporary directory'));
   }
 
-  //Log
-  console.log(
-    'Installing plugin',
-    chalk.magenta(plugin)
-  );
+  //Get repo URL and log
+  var repoUrl = getRepoUrl(module);
+  console.log(' - Cloning from', chalk.grey(repoUrl));
+
+  //Clone
+  Git.Clone.clone(repoUrl, tmpDir, {
+    remoteCallbacks: {
+      certificateCheck: function() {
+        return 1;
+      }
+    }
+  }).then(function(repository) {
+    cb(null, tmpDir, repository);
+  }, function(reason) {
+    cb(reason);
+  });
+}
+
+/**
+ * Copy module to cwd
+ */
+function copyToCwd(sourceDir) {
+
+  //Get cwd and log
+  var cwd = process.cwd();
+  console.log(' - Copying to', chalk.grey(tildify(cwd)));
+
+  //Ignored files
+  var ignore = ['node_modules', '.git'],
+      sourceLength = sourceDir.length;
+
+  //Copy
+  cpr(sourceDir, cwd, {
+    deleteFirst: false,
+    overwrite: false,
+    confirm: true,
+    filter: function(file) {
+      file = file.substr(sourceLength + 1);
+      for (var i = 0; i < ignore.length; i++) {
+        if (file.indexOf(ignore[i]) === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }, function(error) {
+    if (error) {
+      return cb(error);
+    }
+    cb(null);
+  });
+}
+
+/**
+ * Install several modules in series
+ */
+function installSeries(modules, cb) {
+
+  //Nothing to do?
+  if (!Array.isArray(modules) || modules.length === 0) {
+    return cb(new Error('Invalid list of modules given'));
+  }
+
+  //Get module
+  var module = modules.shift();
+
+  //Run install
+  Meanie.install(module, function(error) {
+
+    //Plugin failed to install
+    if (error) {
+      console.error(chalk.red('Failed to install module'));
+      return cb(error);
+    }
+
+    //Plugin installed! Go for the next one
+    if (modules.length > 0) {
+      installSeries(modules, cb);
+    }
+  });
+}
+
+/*****************************************************************************
+ * Public API
+ ***/
+
+/**
+ * Meanie CLI helper
+ */
+var Meanie = {
+
+  /**
+   * Create Meanie project (installs core modules)
+   */
+  create: function(cb) {
+    cb = cb || function() {};
+
+    //Log
+    console.log(
+      chalk.magenta('Meanie'), 'is creating a new project in',
+      chalk.magenta(tildify(process.cwd()))
+    );
+
+    //Install core modules
+    Meanie.install([
+      'core', 'hond'
+    ], function(error) {
+
+      //Failure
+      if (error) {
+        console.error(chalk.red('Failed to create project'));
+        console.error(chalk.red(error));
+        cb(error);
+        return;
+      }
+
+      //Success
+      console.log(chalk.green('Meanie project created successfully'));
+      console.log(chalk.grey('Run npm install to install dependencies'));
+      cb(null);
+    });
+  },
+
+  /**
+   * Install a meanie module
+   */
+  install: function(module, cb) {
+    cb = cb || function() {};
+
+    //Array of modules given
+    if (Array.isArray(module)) {
+      return installSeries(modules, cb);
+    }
+
+    //Log
+    console.log(chalk.magenta('Meanie'), 'is installing module', chalk.magenta(module));
+
+    //Clone
+    cloneFromRepo(module, function(error, tmpDir) {
+
+      //Failed to clone
+      if (error) {
+        return cb(error);
+      }
+
+      //Copy contents from temp dir to cwd
+      copyToCwd(tmpDir, function(error) {
+
+        //Failed to copy
+        if (error) {
+          return cb(error);
+        }
+
+        //Plugin was installed successfully
+        console.log(chalk.green('Plugin'), chalk.magenta(module), chalk.green('installed'));
+        cb(null, module);
+      });
+    });
+  }
 };
 
 /**
- * Return new Meanie instance
+ * Export module
  */
-module.exports = new Meanie();
+module.exports = Meanie;
